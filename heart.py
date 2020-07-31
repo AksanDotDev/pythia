5ee63a544ee3e6e09381c0192df3d0d1d37e110c
Alexi Dewfall <git@aksan.dev>
2020-07-31 18:57:51 +0100

    Linting update

--- heart.py
+++ heart.py
@@ -6,16 +6,18 @@ from config import config
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('Online as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    if message.content.lower() ==  "marco":
+
+    if message.content.lower() == "marco":
         await message.channel.send("Polo!")
 
     if client.user.mentioned_in(message):
