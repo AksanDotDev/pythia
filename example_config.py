45ad1979a16a016d0db4aafe346f7ba5f5bb3144
Alexander Dewfall <git@aksan.dev>
2020-10-15 21:05:23 +0100

    Fixing example to pass flake8

--- example_config.py
+++ example_config.py
@@ -2,18 +2,18 @@ from munch import Munch
 
 params = {
     "discord": {
-        "token": FromDiscord,
+        "token": "FromDiscord",
     },
     "hosts": {
         "hostname": {
-            "MAC": MacAddress,
-            "IPv4": IPv4Address,
-            "URL": URL,
-            "IPv6": IPv6Address,
+            "MAC": "MacAddress",
+            "IPv4": "IPv4Address,
+            "URL": "URL",
+            "IPv6": "IPv6Address",
         }
     },
     "network": {
-        "broadcast": BroadcastIP,
+        "broadcast": "BroadcastIP",
     },
 }
 
