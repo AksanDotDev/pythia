8b38b9b8bb13726f82bc9aa141448f02c4324d95
Alexander Dewfall <git@aksan.dev>
2020-10-15 21:06:50 +0100

    Final fix to pass flake8

--- example_config.py
+++ example_config.py
@@ -7,7 +7,7 @@ params = {
     "hosts": {
         "hostname": {
             "MAC": "MacAddress",
-            "IPv4": "IPv4Address,
+            "IPv4": "IPv4Address",
             "URL": "URL",
             "IPv6": "IPv6Address",
         }
